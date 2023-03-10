e032b271c7a10fdae32ba10a3ceb5a7edcead768
ayush <ayushjaswal4543@gmail.com>
2023-03-10 15:25:55 +0530

    Added KNN method

--- scikit-learn/KNN/KNN.py
+++ scikit-learn/KNN/KNN.py
@@ -9,3 +9,47 @@ import pandas as pd
 from sklearn import neighbors, metrics
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+
+data = pd.read_csv('car.data')
+
+x = data[[
+    'buying',
+    'maint',
+    'saftey'
+]].values
+y = data[['class']]
+# converting the data for x
+
+Le = LabelEncoder()
+for i in range(len(x[0])):
+    x[:, i] = Le.fit_transform(x[:, i])
+
+# converting the data for y
+
+label_mapping = {
+    'unacc': 0,
+    'acc': 1,
+    'good': 2,
+    'vgood': 3
+}
+
+y['class'] = y['class'].map(label_mapping)
+y = np.array(y)
+
+#CREATING A MODEL
+
+knn = neighbors.KNeighborsClassifier(n_neighbors=25, weights='uniform')
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+knn.fit(x_train, y_train)
+
+prediction = knn.predict(x_test)
+
+accuracy = metrics.accuracy_score(y_test, prediction);
+print("Predictions: ", prediction)
+print("Accuracy: ", accuracy)
+
+a = 56
+print("Actual Value: ", y[a])
+print("Predicted Value: ", knn.predict(x)[a])
